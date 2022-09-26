a2894adfe99cb973de900261234f7b0498645356
MrZhao01 <114091970+MrZhao01@users.noreply.github.com>
2022-09-26 20:13:47 +0800

    Add files via upload

---
+++ 给定条件的整数集.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+/*#include<iostream>
+using namespace std;
+
+int main() {
+	int a;
+	int cot = 0;
+	do {
+		printf("请输入小于6的正整数：");
+		scanf("%d", &a);
+	} while (a>=6);
+	for (int i = a; i <= a+3;i++) {
+		for (int j = a; j <= a + 3; j++) {
+			for (int k = a; k <= a + 3; k++) {
+				if (i != j && i != k && j != k) {
+					printf("%d%d%d  ", i, j, k);
+					cot++;
+					if (cot % 6 == 0) {
+						printf("\n");
+					}
+				}
+			}
+		}
+	}
+
+	system("pause");
+	return 0;
+}*/
